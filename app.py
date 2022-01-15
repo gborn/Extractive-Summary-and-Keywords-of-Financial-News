5f5d40170444293f7484474f520e954690d93260
Glad B <gborn.new@gmail.com>
2022-01-15 15:57:48 +0530

    shift to accelerated inference to save memory

--- app.py
+++ app.py
@@ -7,15 +7,8 @@ import os
 PAGE_CONFIG = {"page_title":"App by Glad Nayak","page_icon":":white_check_mark:","layout":"wide"}
 st.set_page_config(**PAGE_CONFIG)
 
-def main():
-    """
-    Render UI on web app
-    """
 
-    # load model
-    model = Model()
-    print('loaded all models and tokenizers')
-    
+def render_textbox():
     # adding the text that will show in the text box as default
     default_value = """
     LONDON, Jan 7 (Reuters) - Bitcoin slumped as much as 5% on Friday to its lowest since late September, amid a broader sell-off for cryptocurrencies driven by concerns about tighter U.S. monetary policy. Bitcoin was last down more than 3% at $41,704 after touching $40,938, its lowest since Sept. 29, as a mixed bag of U.S. payrolls data fuelled some bargain buying. The world's biggest cryptocurrency has lost over 40% since hitting a record high of $69,000 in November and the volatility that has plagued it since its birth 13 years ago remains stubbornly present. The global computing power of the bitcoin network has dropped sharply this week following the shutdown of Kazakhstan's internet as an uprising hit the country's fast-growing cryptocurrency mining industry. Bitcoin has also been under pressure after minutes from the latest U.S. Federal Reserve meeting, released on Wednesday, appeared to lean toward more aggressive policy action, sapping investor appetite for riskier assets. "We are seeing broad risk-off sentiment across all markets currently as inflationary concerns and rate hikes appear to be at the forefront of speculators' minds," said Matthew Dibb, COO of Singapore crypto platform Stack Funds. "Liquidity in BTC has been quite thin on both sides and there is risk of a retreat back to the mid-30's on the short term." Ether , the second largest token by market cap, fell as much as 8.6% to $3,114, its lowest since Oct. 1. It was last trading down more than 6% at $3,200.
@@ -23,7 +16,9 @@ def main():
 
     st.title("Abstractive Summary and Keyword Extraction of Financial News")
     text = st.text_area("Paste an article to get started 🤗", default_value.strip(), height = 275)
+    return text
 
+def render_sentiment(model, text):
     if text.strip():
         label, score = model.get_sentiment(text)
         if label == 'POSITIVE':
@@ -32,17 +27,45 @@ def main():
         else:
             st.write(f':cry: This article has a {label.lower()} sentiment. Confidence score: {score:.2f}')
 
-        keywords, scores = model.get_keywords(text, with_highlight=False)
-        st.subheader(f'Suggested Keywords:')
-        st.write(', '.join(keywords))
 
-        title = model.get_title(text)
-        st.subheader(f'Suggested Headline:')
-        st.write(title)
+def render_keywords(model, text):
+    keywords, scores = model.get_keywords(text, with_highlight=False)
+    st.subheader(f'Suggested Keywords:')
+    st.write(', '.join(keywords))
 
-        summary = model.get_summary(text)
-        st.subheader("And here's the abstract:")
-        st.write(summary)
+def render_title(model, text):
+    title = model.get_title(text)
+    st.subheader(f'Suggested Headline:')
+    st.write(title)
+
+def render_summary(model, text):
+    summary = model.get_summary(text)
+    st.subheader("And here's the abstract:")
+    st.write(summary)
+
+def main():
+    """
+    Render UI on web app
+    """
+    # render input textbox
+    text = render_textbox()
+
+    # load model
+    model = Model()
+    print('loaded all models and tokenizers')
+
+    # display sentiment
+    render_sentiment(model, text)
+
+    # render suggested keywords section
+    render_keywords(model, text)
+
+    # render suggested title section
+    render_title(model, text)
+
+    # render abstract summary section
+    render_summary(model, text)
+
 
 if __name__ == '__main__':
     main()
--- src/model.py
+++ src/model.py
@@ -1,25 +1,45 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+from transformers import pipeline
 from sentence_transformers import SentenceTransformer
 from keybert import KeyBERT
 import re
+import requests
+import json
+import os
+
+# token to authorize with Hugging Face
+API_URL = "https://api-inference.huggingface.co/models/gborn/autonlp-news-summarization-483413089"
+
+with open('.env') as f:
+    API_TOKEN=str(f.read()).strip('\n')
+    
+headers = {"Authorization": f"{API_TOKEN}"}
+
+def query(payload, API_URL=API_URL, headers=headers):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    output =  json.loads(response.content.decode("utf-8"))
+    return output
 
 class Model:
     def __init__(self, ):
-        # we use Google’s T5 model which was pre-trained on a multi-task mixed dataset.
-        self.summarizer = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
-        self.summarize_tokenizer = AutoTokenizer.from_pretrained("t5-base")
-
         # To generate title, we use AutoNLP model trained on Reuters dataset present in our data folder
-        self.title_tokenizer = AutoTokenizer.from_pretrained("gborn/autonlp-news-summarization-483413089")
-        self.title_model = AutoModelForSeq2SeqLM.from_pretrained("gborn/autonlp-news-summarization-483413089")
+        # Instead of loading model, we will use accelerated inference from HuggingFace
+        # ping model to wake it up
+        output = query({"inputs": "The answer to the universe is"})
+        if isinstance(output, dict):
+            print("Estimated time to load glad's summarizer app", output)
+
+
+        # sentiment analysis
+        self.classifier = pipeline("sentiment-analysis")
+        print('Loaded sentiment classification model')
+
+        # we use Google’s T5 model which was pre-trained on a multi-task mixed dataset.
+        self.summarizer = pipeline("summarization")
+        print('Loaded summarizer')
 
         # KeyBert uses BERT-embeddings and simple cosine similarity to find the sub-phrases in a document that are the most similar to the document itself.
         sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
         self.kw_model = KeyBERT(sentence_model)
-
-        # sentiment analysis
-        self.classifier = pipeline("sentiment-analysis")
-        print('Loaded models')
 
     def get_sentiment(self, text):
         result = self.classifier(text)[0]
@@ -36,23 +56,22 @@ class Model:
     def get_title(self, text):
         # The model eats a lot of memory, so we will use only first 1000 characters
         text = text[:1000]
-        features = self.title_tokenizer(text, return_tensors='pt')
-        tokens = self.title_model.generate(input_ids=features['input_ids'])[0]
-        output = self.title_tokenizer.decode(tokens, skip_special_tokens=True)
-        return output.strip()
+        output = query({"inputs": text})
+        return output[0]['generated_text']
 
     def get_summary(self, text):
         # T5 uses a max_length of 512 so we cut the article to 512 tokens.
-        inputs = self.summarize_tokenizer("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
-        outputs = self.summarizer.generate(
-             inputs["input_ids"], 
-             max_length=150, 
-             min_length=40, 
-             length_penalty=2.0, 
-             num_beams=4, 
-             early_stopping=True
-        )
-        return self.summarize_tokenizer.decode(outputs[0], skip_special_tokens=True)
+        # inputs = self.summarize_tokenizer("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
+        # outputs = self.summarizer.generate(
+        #      inputs["input_ids"], 
+        #      max_length=150, 
+        #      min_length=40, 
+        #      length_penalty=2.0, 
+        #      num_beams=4, 
+        #      early_stopping=True
+        # )
+        output = self.summarizer(text, max_length=512, min_length=30, do_sample=False)
+        return output[0]['summary_text']
 
     def clean_text(self, text):
         text = text.replace(u"\ufffd", "")
